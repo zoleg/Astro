ead2a83383fe6be395fd12bd0d45bd163322c16a
Administrator <gitlab_admin_c167fb@example.com>
2025-02-27 03:41:32 +0000

    Edit astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ export default {
     sitemap: true,         // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
+    hostname: 'localhost',  // The hostname to run the dev server on.
+    port: 3000,             // The port to run the dev server on.
   },
   renderers: [
     "@astrojs/renderer-svelte"
